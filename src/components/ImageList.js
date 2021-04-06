6dac0e83a9d4f8d28ef303d61d2593fb978250fb
yash1000 <yashsanja1@gmail.com>
2021-04-07 00:28:06 +0530

    complete api calls

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,12 @@
 import React from "react";
+import './ImageList.css'
+import ImageCard from "./ImageCard";
 
-const ImageList =() => {
-    return <div>list</div>
+const ImageList =(props) => {
+    const img = props.images.map((img) => {
+        return <ImageCard image={img} key={img.id}/>
+    })
+    return <div className={'image-list'}>{img}</div>
 }
 
 export default ImageList;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,7 +6,6 @@ class SearchBar extends React.Component {
     }
     onFormSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state.term)
         this.props.onSubmit(this.state.term)
     }
 
